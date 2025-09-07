7cfe7f98060aa4a6f9bfbd27b4d2fcb28d699713
pktolstov <pktolstov@gmail.com>
2025-09-07 21:41:30 +0300

    Improve next.config.js for cloudfare deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-module.exports = {
-    output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      runtime: 'edge', // Опционально, если хочешь Edge Functions
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      unoptimized: true, // важно для Cloudflare, чтобы Image компонент работал без node server
+    },
   };
   
+  module.exports = nextConfig;
