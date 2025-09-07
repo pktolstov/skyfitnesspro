7c1839361727156776d87c3fc18614a2070d259b
pktolstov <pktolstov@gmail.com>
2025-09-07 23:13:46 +0300

    Changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      runtime: 'edge', // Опционально, если хочешь Edge Functions
-    },
     reactStrictMode: true,
     swcMinify: true,
     images: {
