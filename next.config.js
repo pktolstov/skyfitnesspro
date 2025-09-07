49270390259272a92c48e859229576a3e41053c6
pktolstov <pktolstov@gmail.com>
2025-09-07 23:36:21 +0300

    Improve next.config.js to redirect /fitness/main

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      runtime: 'edge', // Опционально, если хочешь Edge Functions
-    },
     reactStrictMode: true,
     swcMinify: true,
     images: {
-      unoptimized: true, // важно для Cloudflare, чтобы Image компонент работал без node server
+      unoptimized: true, // нужно для Cloudflare
+    },
+    experimental: {
+      runtime: 'edge', // если используешь Edge Functions
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/fitness/main',
+          permanent: true,
+        },
+      ];
     },
   };
   
